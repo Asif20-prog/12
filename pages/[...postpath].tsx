b664a35459ac4a43a694943ec142293f9360670e
Asif20-prog <kntktnsa@gmail.com>
2025-02-20 17:27:00 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://paktv-f45e89.ingress-erytho.ewp.live/graphql"
+	const endpoint = "https://oscam.space/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
